663c779b9643441504c23bc782fe30108ba90a96
Vitor Souza <vitor.souza@absoftware.com.br>
2023-04-11 13:03:23 -0300

    Adding docker-compose.yml

--- producer-kafka/src/main/java/br/com/souza/producerkafka/config/KafkaTopicConfig.java
+++ producer-kafka/src/main/java/br/com/souza/producerkafka/config/KafkaTopicConfig.java
@@ -3,16 +3,13 @@ package br.com.souza.producerkafka.config;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
 
     @Bean
     public NewTopic topic(){
-        return TopicBuilder
-                .name("souza-topic")
-                .build();
+        return new NewTopic("souza-topic", 3, (short) 1);
     }
 
 }
